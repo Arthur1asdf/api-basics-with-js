520fe9ea356e36a2efce75f01fed239a122d9573
Arthur1asdf <arthurteng0819@gmail.com>
2024-07-12 23:25:28 -0400

    finished

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ foxBtn.addEventListener("click", getRandomFox);
 dogBtn.addEventListener("click", getRandomDog);
 
 function getRandomFox() {
-  fetch("https://random-d.uk/api/random")
+  fetch("https://randomfox.ca/floof/")
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      foxResult.innerHTML = `<img src= "${data.url}"/>`;
+      foxResult.innerHTML = `<img src= "${data.image}"/>`;
     })
     .catch((error) => {
       console.error("Error fetching fox image:", error);
     });
 }
 function getRandomDog() {
-  fetch("https://randomfox.ca/floof/")
+  fetch("https://random.dog/woof.json")
     .then((res) => res.json())
     .then((data) => {
       dogResult.innerHTML = `<img src= "${data.url}"/>`;
